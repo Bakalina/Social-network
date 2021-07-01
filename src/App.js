64d9d3ad909666f509ef2ec13ac25a77707bc7e7
bakalina.yaroslav@gmail.com <bakalina.yaroslav@gmail.com>
2021-07-01 11:59:27 +0300

    Btn + TableAuthors 0.1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
+import {Col, Container, Row} from "react-bootstrap";
+import Btn from "./Components/ButtonAuthors";
+import TableAuthors from "./Components/TableAuthors";
 
 function App() {
-  return (
-    <div>
-      hello world
-    </div>
-  );
+    return (
+            <Container>
+                <h1>BOOKS</h1>
+                <Row>
+                    <Col>
+                        <Btn name={'Авторы'}/>{' '}
+                        <Btn name={'Жанры'}/>
+                    </Col>
+                </Row>
+                <Row>
+                    <TableAuthors/>
+                </Row>
+            </Container>
+    );
 }
 
 export default App;
