ffb9574edf4b2aa56b90e89a024f29e019f133a4
bakalina.yaroslav@gmail.com <bakalina.yaroslav@gmail.com>
2021-09-22 21:37:54 +0300

    start new project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import React from "react";
-import {Col, Container, Row} from "react-bootstrap";
-import Btn from "./Components/ButtonAuthors";
-import TableAuthors from "./Components/TableAuthors";
 
-function App() {
+
+
+export default function App(){
+
     return (
-            <Container>
-                <h1>BOOKS</h1>
-                <Row>
-                    <Col>
-                        <Btn name={'Авторы'}/>{' '}
-                        <Btn name={'Жанры'}/>
-                    </Col>
-                </Row>
-                <Row>
-                    <TableAuthors/>
-                </Row>
-            </Container>
-    );
-}
+        <div>Hello!!!</div>
+    )
 
-export default App;
+};
