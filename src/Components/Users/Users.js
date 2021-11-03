76fd31f0f33a53eea7f803e677b85dbb82f3fec8
bakalina.yaroslav@gmail.com <bakalina.yaroslav@gmail.com>
2021-11-03 23:53:50 +0200

    API users

--- src/Components/Users/Users.js
+++ src/Components/Users/Users.js
@@ -1,13 +1,19 @@
 import React from "react";
+import * as axios from "axios";
 
 
 export default function Users(props) {
+
+    axios.get('https://social-network.samuraijs.com/api/1.0/users')
+        // eslint-disable-next-line react/prop-types
+        .then((responce) => {props.setUsers(responce.data.items)})
+
     return <div>
         {/* eslint-disable-next-line react/prop-types */}
         {props.users.map(el =>
             <div key={el.id}>
                 <span>
-                    <div><img width='100px' alt='image' src={el.photoUrl}/></div>
+                    <div><img  alt='image' src={el.photos.small}/></div>
                     <div>
                         { el.followed
                             // eslint-disable-next-line react/prop-types
@@ -18,12 +24,12 @@ export default function Users(props) {
                 </span>
                 <span>
                     <span>
-                        <div>{el.fullName}</div>
+                        <div>{el.name}</div>
                         <div>{el.status}</div>
                     </span>
                     <span>
-                        <div>{el.location.city}</div>
-                        <div>{el.location.country}</div>
+                        <div>{'el.location.city'}</div>
+                        <div>{'el.location.country'}</div>
                     </span>
                 </span>
             </div>)}
