f99a091e8de3133e28ecb338291a6c6859ae6238
bakalina.yaroslav@gmail.com <bakalina.yaroslav@gmail.com>
2021-11-06 21:30:58 +0200

    refactor Users.js to Class

--- src/Components/Users/Users.js
+++ src/Components/Users/Users.js
@@ -2,48 +2,117 @@ import React from "react";
 import * as axios from "axios";
 
 
-export default function Users(props) {
-    let getUsers = () => {
-// eslint-disable-next-line react/prop-types
-        if (props.users.length === 0) {
+class Users extends React.Component {
 
+    constructor(props) {
+        super(props);
             axios.get('https://social-network.samuraijs.com/api/1.0/users')
-                // eslint-disable-next-line react/prop-types
                 .then(responce => {
                     // eslint-disable-next-line react/prop-types
-                    props.setUsers(responce.data.items)
-                })
+                    this.props.setUsers(responce.data.items)})
+    }
+
+    // getUsers  = () => {
+    //     // eslint-disable-next-line react/prop-types
+    //     if (this.props.users.length === 0) {
+    //         axios.get('https://social-network.samuraijs.com/api/1.0/users')
+    //             .then(responce => {
+    //                 // eslint-disable-next-line react/prop-types
+    //                 this.props.setUsers(responce.data.items)
+    //             })
+    //         }
+    //     }
+
+
+    render() {
+        return <div>
+            <button onClick={this.getUsers}>Get Users</button>
+            {/* eslint-disable-next-line react/prop-types */}
+            {this.props.users.map(el =>
+                <div key={el.id}>
+                    <span>
+                        <div><img width='70px' alt='image'
+                                  src={el.photos.small != null
+                                      ? el.photos.small
+                                      : 'https://klike.net/uploads/posts/2019-03/1551511801_1.jpg'}/></div>
+                        <div>
+                            {el.followed
+                                // eslint-disable-next-line react/prop-types
+                                ? <button onClick={() => {this.props.unFollow(el.id)}}>Unfollow</button>
+                                // eslint-disable-next-line react/prop-types
+                                : <button onClick={() => {this.props.follow(el.id)}}>Follow</button>}
+                        </div>
+                    </span>
+                    <span>
+                        <span>
+                            <div>{el.name}</div>
+                            <div>{el.status}</div>
+                        </span>
+                        <span>
+                            <div>{'-'}</div>
+                            <div>{'-'}</div>
+                        </span>
+                    </span>
+                </div>)}
+            </div>
         }
     }
 
+export default Users;
 
-    return <div>
-        <button onClick={getUsers}>Get Users</button>
-        {/* eslint-disable-next-line react/prop-types */}
-        {props.users.map(el =>
-            <div key={el.id}>
-                <span>
-                    <div><img width='70px' alt='image' src={el.photos.small != null ? el.photos.small: 'https://klike.net/uploads/posts/2019-03/1551511801_1.jpg'}/></div>
-                    <div>
-                        { el.followed
-                            // eslint-disable-next-line react/prop-types
-                            ? <button onClick={()=>{props.unFollow(el.id)}}>Unfollow</button>
-                            // eslint-disable-next-line react/prop-types
-                            : <button onClick={()=>{props.follow(el.id)}}>Follow</button> }
-                    </div>
-                </span>
-                <span>
-                    <span>
-                        <div>{el.name}</div>
-                        <div>{el.status}</div>
-                    </span>
-                    <span>
-                        <div>{'-'}</div>
-                        <div>{'-'}</div>
-                    </span>
-                </span>
-            </div>)}
-    </div>
+// export default function Users(props) {
+//     let getUsers = () => {
+// eslint-disable-next-line react/prop-types
+// if (props.users.length === 0) {
+//
+//     axios.get('https://social-network.samuraijs.com/api/1.0/users')
+//         eslint-disable-next-line react/prop-types
+// .then(responce => {
+//     eslint-disable-next-line react/prop-types
+// props.setUsers(responce.data.items)
+// })
+// }
+// }
+//
+//
+// return <div>
+//     <button onClick={getUsers}>Get Users</button>
+//     {/* eslint-disable-next-line react/prop-types */}
+//     {props.users.map(el =>
+//         <div key={el.id}>
+//             <span>
+//                 <div><img width='70px' alt='image' src={el.photos.small != null ? el.photos.small: 'https://klike.net/uploads/posts/2019-03/1551511801_1.jpg'}/></div>
+//                 <div>
+//                     { el.followed
+//                         eslint-disable-next-line react/prop-types
+// ? <button onClick={()=>{props.unFollow(el.id)}}>Unfollow</button>
+// eslint-disable-next-line react/prop-types
+// : <button onClick={()=>{props.follow(el.id)}}>Follow</button> }
+// </div>
+// </span>
+// <span>
+//     <span>
+{/*<div>{el.name}</div>*/
+}
+{/*<div>{el.status}</div>*/
+}
+{/*</span>*/
+}
+{/*<span>*/
+}
+{/*    <div>{'-'}</div>*/
+}
+{/*    <div>{'-'}</div>*/
+}
+{/*</span>*/
+}
+{/*</span>*/
+}
+{/*</div>)}*/
+}
+{/*</div>*/
+}
+{/*}*/
 }
 
 
