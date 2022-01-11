0dbd184247a108ac99b266d7e1121279c8fca053
bakalina.yaroslav@gmail.com <bakalina.yaroslav@gmail.com>
2022-01-11 10:50:46 +0200

    create validator Login.js

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,18 +1,29 @@
 import React from "react";
 import {Field, reduxForm} from "redux-form";
+import {Input} from "../common/FormsControls/FormsControls";
+import {required} from "../utils/validators/validators";
 
 
 const LoginForm = (props) => {
     return (
         <form onSubmit={props.handleSubmit}>
             <div>
-                <Field name={'login'} placeholder={'Login'} component={'input'}/>
+                <Field name={'login'}
+                       placeholder={'Login'}
+                       component={Input}
+                       validate={[required]}/>
             </div>
             <div>
-                <Field name={'password'} placeholder={'Password'} component={'input'}/>
+                <Field name={'password'}
+                       placeholder={'Password'}
+                       component={Input}
+                       validate={[required]}/>
             </div>
             <div>
-                <Field name={'rememberMe'} component={'input'} type={'checkbox'} /> remember me
+                <Field name={'rememberMe'}
+                       component={Input}
+                       type={'checkbox'}/>
+                remember me
             </div>
             <div>
                 <button>Login</button>
--- src/Components/common/FormsControls/FormsControls.js
+++ src/Components/common/FormsControls/FormsControls.js
@@ -1,15 +1,25 @@
 import React from "react";
 import style from './FormsControls.module.css'
 
-export const Textarea = ({input, meta, ...props}) => {
+const FormControl = ({input, meta, ...props}) => {
     const showError = meta.touched && meta.error;
 
     return (
         <div className={style.formControl + ' ' + (showError && style.error)}>
             <div>
-                <textarea {...input} {...props}/>
+                {props.children}
             </div>
             {showError && <span>{meta.error}</span>}
         </div>
     )
+}
+
+export const Textarea = (props) => {
+    const {input, ...restProps} = props
+    return <FormControl {...props}><textarea {...input} {...restProps}/></FormControl>
+}
+
+export const Input = (props) => {
+    const {input, ...restProps} = props
+    return <FormControl {...props}><input {...input} {...restProps}/></FormControl>
 }
