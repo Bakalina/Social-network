e18ea30d47643c9531e79a5d1141c0d422693a4d
Yaroslav Bakalyna <bakalyna.yaroslav@gmail.com>
2022-07-03 23:43:47 +0300

    refactoring HeaderContainer

--- src/Components/Header/HeaderContainer.tsx
+++ src/Components/Header/HeaderContainer.tsx
@@ -1,17 +1,24 @@
-import React from "react";
+import React, {FC} from "react";
 import Header from "./Header";
 import {connect} from "react-redux";
 import {logout} from "../../Redux/authReducer";
 import {getIsAuth, getLogin} from "../../Redux/authSelectors";
+import {AppStateType} from "../../Redux/reduxStore";
 
-const HeaderContainer = (props) => {
+type HeaderContainerProps = {
+    isAuth: boolean,
+    login: string | null,
+    logout: () => void
+}
+
+const HeaderContainer: FC<HeaderContainerProps> = (props) => {
 
     return (
         <Header {...props}/>
     );
 };
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = (state: AppStateType) => ({
     isAuth: getIsAuth(state),
     login: getLogin(state)
 });
