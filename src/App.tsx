41aa1f2e5658d3d8c5288e8ae006ba83be5598dc
Yaroslav Bakalyna <bakalyna.yaroslav@gmail.com>
2022-07-05 21:48:06 +0300

    change Route Dialog page

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React, {FC, useEffect} from "react";
 import Nav from "./Components/Nav/Nav";
 import style from "./App.module.css";
 import {Redirect, Route, withRouter} from "react-router-dom";
-import DialogsContainer from "./Components/Dialogs/DialogsContainer";
 import ProfileContainer from "./Components/Profile/ProfileContainer";
 import HeaderContainer from "./Components/Header/HeaderContainer";
 import Login from "./Components/Login/Login";
@@ -12,6 +11,7 @@ import {initializedApp} from "./Redux/appReducer";
 import Preloader from "./Components/common/Preloader/Preloader";
 import {AppStateType} from "./Redux/reduxStore";
 import Users from "./Components/Users/Users";
+import Dialogs from "./Components/Dialogs/Dialogs";
 
 type MapPropsType = ReturnType<typeof mapStateToProps>
 type DispatchPropsType = {
@@ -35,7 +35,7 @@ const App: FC<MapPropsType & DispatchPropsType> = (props) => {
             <div className={style.appContent}>
                 <Route exact path='/' render={()=> <Redirect to={'/profile'}/>}/>
                 <Route path='/profile/:userId?' render={() => <ProfileContainer/>}/>
-                <Route path='/dialogs' render={() => <DialogsContainer/>}/>
+                <Route path='/dialogs' render={() => <Dialogs/>}/>
                 <Route path='/users' render={() => <Users/>}/>
                 <Route path='/login' render={() => <Login/>}/>
             </div>
