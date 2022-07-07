50ea05225162262dbdde2f40dc19dcc0ea105748
Yaroslav Bakalyna <bakalyna.yaroslav@gmail.com>
2022-07-07 23:03:21 +0300

    debug Header.tsx

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -4,7 +4,7 @@ import logo from "../../image/logo22.png";
 import {NavLink} from "react-router-dom";
 import {useDispatch, useSelector} from "react-redux";
 import {getIsAuth, getLogin} from "../../Redux/authSelectors";
-import { logout } from "../../Redux/authReducer";
+import {logout} from "../../Redux/authReducer";
 
 
 const Header = () => {
@@ -14,12 +14,13 @@ const Header = () => {
 
     const dispatch = useDispatch();
 
+
     return (
         <div className={style.head}>
             <img alt={'logo'} src={logo}/>
             <div className={style.loginBlock}>
                 {isAuth ?
-                    <div>{login} - <button className={style.button} onClick={dispatch(logout)}>Log out</button></div>
+                    <div>{login} - <button className={style.button} onClick={() => dispatch(logout())}>Log out</button></div>
                     : <NavLink className={style.login} to={'/login'}>Login</NavLink>}
             </div>
         </div>
